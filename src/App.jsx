5972ff363bfca9f8de2b847afa201fbcea20b278
iayushkumar <kayush395@gmail.com>
2024-01-09 11:15:30 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import OTP from "./pages/OTP";
 import SignUp from "./pages/SignUp";
 // import OTPBox from "./pages/OTPP";
+import Login from "./pages/Login";
 
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      <SignUp />
-      {/* <OTP /> */}
-      {/* <OTPBox /> */}
-    </>
+   
+
+<Router>
+    {/* <Login/> */}
+     <Routes>
+     <Route path="/login" element={<Login/>}/>
+     <Route path="/Signup" element={<SignUp/>}/>
+     <Route path="/otp" element={<OTP/>}/>
+       <Route/>
+     </Routes>
+     </Router>
   );
 }
 
