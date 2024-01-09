4bc39339e1e9c0333792f7a8b56ac51af5260524
Avinash Tallapaneni <avinashtallapaneni@gmail.com>
2024-01-09 09:55:09 +0530

    signup page and otp page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import OTP from "./pages/OTP";
+import SignUp from "./pages/SignUp";
+// import OTPBox from "./pages/OTPP";
+
+function App() {
+  return (
+    <>
+      <SignUp />
+      {/* <OTP /> */}
+      {/* <OTPBox /> */}
+    </>
+  );
+}
+
+export default App;
