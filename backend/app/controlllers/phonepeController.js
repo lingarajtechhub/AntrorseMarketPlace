8b7b0f7aed73e454ca2f1b7fb679c776517bfcd9
Upendra731996 <upendragupta731996@gmail.com>
2024-03-08 11:02:55 +0530

    updatephonepay

--- backend/app/controlllers/phonepeController.js
+++ backend/app/controlllers/phonepeController.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 const axios = require("axios");
 require("dotenv").config();
+const sha256 = require("sha256");
 
 const createNewPayment = async (req, res) => {
   const data = {
@@ -10,22 +11,29 @@ const createNewPayment = async (req, res) => {
     buyerName: req.body.buyerName,
     amount: req.body.amount,
     // redirectUrl: `${process.env.REDIRECT_URL}/api/status/${req.body.transactionId}`,
-    redirectUrl: `${process.env.REDIRECT_URL}/status/${req.body.transactionId}`,
-    redirectMode: "POST",
+    redirectUrl: `${process.env.REDIRECT_URL}/payment/validate/${req.body.transactionId}`,
+    redirectMode: "REDIRECT",
     paymentInstrument: {
       type: "PAY_PAGE",
     },
   };
 
-  const payloadStringify = JSON.stringify(data);
-  const payload = Buffer.from(payloadStringify).toString("base64");
+  // const payloadStringify = JSON.stringify(data);
+  // const payload = Buffer.from(payloadStringify).toString("base64");
 
-  var hash = crypto.createHash("sha256");
-  const SHA256 = hash
-    .update(payload + process.env.PHONEPE_API_ENDPOINT + process.env.SALT_KEYS)
-    .digest("hex");
+  // const hash = crypto.createHash("sha256");
+  // const SHA256 = hash
+  //   .update(payload + process.env.PHONEPE_API_ENDPOINT + process.env.SALT_KEYS)
+  //   .digest("hex");
 
-  const checksum = SHA256 + "###" + process.env.SALT_KEYS_INDEX;
+  // const checksum = SHA256 + "###" + process.env.SALT_KEYS_INDEX;
+
+  let bufferObj = Buffer.from(JSON.stringify(data), "utf8");
+  let payload = bufferObj.toString("base64");
+
+  let string = payload + "/pg/v1/pay" + process.env.SALT_KEYS;
+  let sha256_val = sha256(string);
+  let checksum = sha256_val + "###" + process.env.SALT_KEYS_INDEX;
 
   const options = {
     url: process.env.PHONEPE_TEST_URL,
@@ -44,6 +52,7 @@ const createNewPayment = async (req, res) => {
       headers: options.headers,
     });
 
+    console.log(response.data);
     const data = response.data.data.instrumentResponse.redirectInfo.url;
 
     return res.json({ statusCode: 200, redirectURL: data, message: "success" });
@@ -54,43 +63,36 @@ const createNewPayment = async (req, res) => {
 
 const checkStatus = async (req, res) => {
   const merchantId = process.env.MERCHANT_ID;
-  const transactionId = req.body.transactionId;
+  const transactionId = req.params.transactionId;
+  const string = `/pg/v1/status/${merchantId}/${transactionId}${process.env.SALT_KEYS}`;
 
-  const string =
-    `/pg/v1/status/${merchantId}/${transactionId}` +
-    process.env.SALT_KEYS_INDEX;
-
-  var hash = crypto.createHash("sha256");
-  const SHA256 = hash.update(string).digest("hex");
-  const checksum = SHA256 + "###" + process.env.SALT_KEYS_INDEX;
+  let sha256_val = sha256(string);
+  let checksum = sha256_val + "###" + process.env.SALT_KEYS_INDEX;
 
   const options = {
-    url: `${process.env.PHONEPE_TEST_URL}/status/${merchantId}/${transactionId}`,
+    url: `${process.env.PHONEPE_TEST_STATUS_URL}/status/${merchantId}/${transactionId}`,
     headers: {
-      accept: "application/json",
       "Content-Type": "application/json",
       "X-VERIFY": checksum,
-      "X-MERCHANT-ID": `${merchantId}`,
+      "X-MERCHANT-ID": merchantId,
+      accept: "application/json",
     },
   };
 
-  try {
-    const response = await axios.get(options.url, {
+  axios
+    .get(options.url, {
       headers: options.headers,
+    })
+    .then(function (response) {
+      if (response.data && response.data.code === "PAYMENT_SUCCESS") {
+        res.send(response.data);
+      } else {
+        res.send(response);
+      }
+    })
+    .catch(function (error) {
+      res.send(error);
     });
-
-    console.log(response, "response");
-
-    if (response.data.success === true) {
-      const url = `http://localhost:5173/success`;
-      return res.redirect(url);
-    } else {
-      const url = `http://localhost:5173/failure`;
-      return res.redirect(url);
-    }
-  } catch (error) {
-    return res.json({ statusCode: 500, message: error.message });
-  }
 };
 
 module.exports = {
